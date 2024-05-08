3ba4f86f7c0ba892a57d508aae8690b8b6d9f217
syifaafra <afra.mumt@gmail.com>
2024-05-08 22:53:57 +0700

    w09

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -16,7 +16,7 @@
 # You new to set "REC2" with your own Public-Key Identity!
 # Check it out with "gpg --list-key"
 # ####################### Replace REC2 ####
-REC2="9975F08C5305BC1A"
+REC2="FRA"
 # ####################### ####### #### ####
 # REC1: public key
 REC1="63FB12B215403B20"
